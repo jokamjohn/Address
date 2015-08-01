2ab6c30fda2715eb5516a8fad00ba527f65544fc
jokamjohn <johnkagga@gmail.com>
2015-08-01 13:07:00 +0300

    displaying the object contents

--- demo.php
+++ demo.php
@@ -7,8 +7,22 @@
  */
 require 'Address.inc';
 
-echo '<h2>Instantiating Address</h2>';
-$address = new Address();
-//inspecting the object
-echo '<h2>Empty object</h2>';
+    echo '<h2>Instantiating Address</h2>';
+    $address = new Address();
+    //inspecting the object
+    echo '<h2>Empty object</h2>';
+    echo '<tt><pre>'.var_export($address).'</pre></tt>';
+
+//assigning values
+    $address->countryName = 'Uganda';
+    $address->postalCode = '256';
+    $address->subdivision = 'Mengo';
+    $address->cityName = 'Kampala';
+    $address->streetAddress1 = '1251';
+    $address->streetAddress2 = '9021';
+echo '<h2>Address object</h2>';
 echo '<tt><pre>'.var_export($address).'</pre></tt>';
+
+//calling the dispaly method
+echo '<h2>Display Address</h2>';
+echo $address->display();
